acf44d330fbd12ad30b6b949e2edffff2e72036b
Jinu <dleck28@gmail.com>
2023-08-20 21:37:52 +0900

    230820 BOJ 1183 수학/정렬

---
+++ baekjoon/BOJ1183/1183.cpp
@@ -0,0 +1,29 @@
+/*BOJ 1183*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> v;
+
+int main(){
+    int N;
+    cin >> N;
+
+    for(int i =0; i<N;++i){
+        int a,b;
+        cin >> a >> b;
+        v.push_back(a-b);
+    }
+
+    sort(v.begin(),v.end());
+
+    if(N%2==0){ //짝수이면 가운데 값
+        cout << v[N/2] - v[N/2-1] + 1 << endl;
+    }else{
+        cout << "1" << endl;
+    }
+
+    return 0;
+}
+
